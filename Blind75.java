c06b1bb09f0437825826c2ac8b4f11de16921b92
swooo <liushanting@gmail.com>
2020-10-13 15:21:32 -0400

    Updated with 'Best Time to Buy and Sell Stock'

--- Blind75.java
+++ Blind75.java
@@ -3,9 +3,17 @@ import java.util.*;
 public class Blind75 {
     public static void main(String[] args) {
         System.out.println("This is Team Blind's 75 problems.");
+
+        /* Test twoSum()
         int[] numsTest = new int[]{2, 7, 11, 15};
         int target = 9;
         System.out.println(Arrays.toString(twoSum(numsTest, target)));
+        */
+
+        /* Test maxProfit()
+        int[] priceTest = new int[]{7, 1, 5, 3, 6, 4};
+        System.out.println(maxProfit(priceTest));
+        */
     }
 
     private static int[] twoSum(int[] nums, int target){
@@ -22,4 +30,19 @@ public class Blind75 {
         }
         throw new IllegalArgumentException("No two sum solution");
     }
+
+    private static int maxProfit(int[] prices){
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int i = 0; i < prices.length; i++){
+            if (prices[i] < minPrice) {
+                minPrice = prices[i];
+            } else if (prices[i] - minPrice > maxProfit){
+                maxProfit = prices[i] - minPrice;
+            }
+        }
+        return maxProfit;
+    }
+
 }
